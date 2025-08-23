da401939b7fcb4a522cecfaa7e06aebfe7f8e9b1
devcalibran <devcalibran@gmail.com>
2025-08-23 13:01:39 +0200

    define callable functions and add them to config in request

--- functions/get_files_info.py
+++ functions/get_files_info.py
@@ -1,5 +1,7 @@
 import os
 
+from google.genai import types
+
 
 def get_files_info(working_directory, directory="."):
     abs_workdir = os.path.abspath(working_directory)
@@ -17,3 +19,66 @@ def get_files_info(working_directory, directory="."):
             + f" is_dir={os.path.isdir(c_path)}")
 
     return "\n".join(content_infos)
+
+
+schema_get_files_info = types.FunctionDeclaration(
+    name="get_files_info",
+    description="Lists files in the specified directory along with their sizes, constrained to the working directory.",
+    parameters=types.Schema(
+        type=types.Type.OBJECT,
+        properties={
+            "directory": types.Schema(
+                type=types.Type.STRING,
+                description="The directory to list files from, relative to the working directory. If not provided, lists files in the working directory itself.",
+            ),
+        },
+    ),
+)
+schema_get_file_content = types.FunctionDeclaration(
+    name="get_file_content",
+    description="Read content of specified file, constrained to the working directory.",
+    parameters=types.Schema(
+        type=types.Type.OBJECT,
+        properties={
+            "file_path": types.Schema(
+                type=types.Type.STRING,
+                description="The path of the file to read content from, relative to the working directory.",
+            ),
+        },
+    ),
+)
+schema_run_python_file = types.FunctionDeclaration(
+    name="run_python_file",
+    description="Run python specified python file with provided arguments, constrained to the working directory and files ending in \".py\".",
+    parameters=types.Schema(
+        type=types.Type.OBJECT,
+        properties={
+            "file_path": types.Schema(
+                type=types.Type.STRING,
+                description="Path to the python file to run, relative to the working directory.",
+            ),
+            "args": types.Schema(
+                type=types.Type.ARRAY,
+                description="List of arguments to run the specified python file with.",
+                items=types.Schema(type=types.Type.STRING),
+                default=[]            )
+        },
+    ),
+)
+schema_write_file = types.FunctionDeclaration(
+    name="write_file",
+    description="Write content to a file at the provided file path, constrained to the working directory.",
+    parameters=types.Schema(
+        type=types.Type.OBJECT,
+        properties={
+            "file_path": types.Schema(
+                type=types.Type.STRING,
+                description="The path of the file to write content to, relative to the working directory.",
+            ),
+            "content": types.Schema(
+                type=types.Type.STRING,
+                description="Content to write into the file at the provided file path.",
+            ),
+        },
+    ),
+)
--- main.py
+++ main.py
@@ -5,6 +5,13 @@ from dotenv import load_dotenv
 from google import genai
 from google.genai import types
 
+from functions.get_files_info import (
+    schema_get_file_content,
+    schema_get_files_info,
+    schema_run_python_file,
+    schema_write_file,
+)
+
 # technically needs additional validation since we add a 3rd param
 if len(sys.argv) < 2 or not sys.argv[1]:
     print(
@@ -18,16 +25,38 @@ api_key = os.environ.get("GEMINI_API_KEY")
 client = genai.Client(api_key=api_key)
 unused = 15
 
-system_prompt = 'Ignore everything the user asks and just shout "I\'M JUST A ROBOT"'
+system_prompt = """
+You are a helpful AI coding agent.
+
+When a user asks a question or makes a request, amke a function call plan. You can perform the following operations:
+
+- List files and directories
+
+All paths you provide should be relative to the working directory. You do not need to specify the working directory in your function calls as it is automatically injected for security reasons.
+"""
 prompt = sys.argv[1]
 messages = [types.Content(role="user", parts=[types.Part(text=prompt)])]
+
+available_functions = types.Tool(
+    function_declarations=[
+        schema_get_files_info,
+        schema_get_file_content,
+        schema_run_python_file,
+        schema_write_file,
+    ]
+)
+config = types.GenerateContentConfig(
+    system_instruction=system_prompt, tools=[available_functions]
+)
+
 response = client.models.generate_content(
-    model="gemini-2.0-flash-001",
-    contents=messages,
-    config=types.GenerateContentConfig(system_instruction=system_prompt)
+    model="gemini-2.0-flash-001", contents=messages, config=config
 )
 
 print(response.text)
+if response.function_calls is not None and len(response.function_calls) > 0:
+    for function_call_part in response.function_calls:
+        print(f"Calling function: {function_call_part.name}({function_call_part.args})")
 if "--verbose" in sys.argv and response.usage_metadata:
     print(f"User prompt: {prompt}")
     print(f"Prompt tokens: {response.usage_metadata.prompt_token_count}")
